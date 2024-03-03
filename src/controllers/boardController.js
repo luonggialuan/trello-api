040917e427726edcd7e4ced724a9d41748328ca3
luonggialuan <luonggialuan07402200@gmail.com>
2024-03-03 16:33:34 +0700

    Error handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -1,16 +1,16 @@
 import { StatusCodes } from 'http-status-codes'
+import ApiError from '~/utils/ApiError'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body:', req.body)
+    // Điều hướng dữ liệu sang tầng Services
 
+    // Có kết quả thì trả về phía Client
     res
       .status(StatusCodes.CREATED)
       .json({ message: 'POST from Controller: API create new board' })
   } catch (error) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-      errors: error.message
-    })
+    next(error)
   }
 }
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import exitHook from 'async-exit-hook'
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
+import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware'
 
 const START_SERVER = () => {
   const app = express()
@@ -13,9 +14,8 @@ const START_SERVER = () => {
   // Use APIs V1
   app.use('/v1', APIs_V1)
 
-  app.get('/', async (req, res) => {
-    res.end('<h1>Hello World!</h1><hr>')
-  })
+  // Middleware xử lý lỗi tập trung
+  app.use(errorHandlingMiddleware)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     //   eslint-disable-next-line no-console
