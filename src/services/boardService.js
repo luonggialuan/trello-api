feaffb1a9aace4e9d4b1b0b393a1e9a8fd8c855c
luonggialuan <luonggialuan07402200@gmail.com>
2024-03-03 21:43:53 +0700

    Create api get board details

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,7 @@
 import { slugify } from '~/utils/formatter'
 import { boardModel } from '~/models/boardModel'
+import ApiError from '~/utils/ApiError'
+import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
@@ -24,7 +26,22 @@ const createNew = async (reqBody) => {
     throw error
   }
 }
+const getDetails = async (boardId) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    // Gọi tới tầng Model để xử lý lưu bản ghi newBoard vào trong Database
+    const board = await boardModel.getDetails(boardId)
+    if (!board) {
+      throw new ApiError(StatusCodes.NOT_FOUND, 'Board not found')
+    }
+
+    return board
+  } catch (error) {
+    throw error
+  }
+}
 
 export const boardService = {
-  createNew
+  createNew,
+  getDetails
 }
