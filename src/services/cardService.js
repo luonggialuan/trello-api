d897c7f99856429c355bc37b489e156476570f82
luonggialuan <luonggialuan07402200@gmail.com>
2024-03-04 20:06:05 +0700

    Create 2 APIs Column & Card

---
+++ src/services/cardService.js
@@ -0,0 +1,24 @@
+import { cardModel } from '~/models/cardModel'
+
+const createNew = async (reqBody) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const newCard = {
+      ...reqBody
+    }
+    const createdCard = await cardModel.createNew(newCard)
+    const getNewCard = await cardModel.findOneById(
+      createdCard.insertedId.toString()
+    )
+
+    //
+
+    return getNewCard
+  } catch (error) {
+    throw error
+  }
+}
+
+export const cardService = {
+  createNew
+}
---
+++ src/services/columnService.js
@@ -0,0 +1,24 @@
+import { columnModel } from '~/models/columnModel'
+
+const createNew = async (reqBody) => {
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const newColumn = {
+      ...reqBody
+    }
+    const createdColumn = await columnModel.createNew(newColumn)
+    const getNewColumn = await columnModel.findOneById(
+      createdColumn.insertedId.toString()
+    )
+
+    //
+
+    return getNewColumn
+  } catch (error) {
+    throw error
+  }
+}
+
+export const columnService = {
+  createNew
+}
