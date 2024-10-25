4009f7dcb7d276889c61301f03bf349c4b638bed
luonggialuan <luonggialuan07402200@gmail.com>
2024-10-25 13:55:03 +0700

    Updates WHITELIST_DOMAINS again

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 // Những domain được phép truy cập tới tài nguyên của Server
 export const WHITELIST_DOMAINS = [
-  'https://trello-web-sable.vercel.app/'
+  'https://trello-web-sable.vercel.app'
   // Không cần config locahost nữa vì đã config ở file cors.js luôn luôn cho phép môi trường dev
   // 'http://localhost:5173'
   // ... thêm domain cho phép truy cập tài nguyên
